4e267e271cb34674bc31a0f165088ea00074cf8d
wayne <275434754@qq.com>
2016-11-23 16:27:20 +0800

    nodejstest

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2016/11/23.
+ */
+
+var http = require('http')
+var fs = require('fs')
+
+//console.log(Object.keys(http))
+var port = process.env.PORT || 8888;
+
+var server = http.createServer(function(request, response){
+    if(request.url === '/'){
+        response.writeHead(200)
+        fs.readFile('index.html', function(error, content){
+            htmlString = content.toString()
+            response.end('<head><meta charset="utf-8"><head> Ï¹¸Ä')
+        })
+    }else {
+        response.writeHead(404)
+        response.end()
+    }
+})
+
+server.listen(port)
+
+console.log('¼àÌý³É¹¦')
